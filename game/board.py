dbcc516e4acecaa9b67581e1a6e3dfd635c94c16
Zorrero Joaquín <j.zorrero@alumno.um.edu.ar>
2023-09-01 17:40:33 -0300

    Calculate word and letter multiplier

--- game/board.py
+++ game/board.py
@@ -2,12 +2,13 @@
 from game.tilebag import Tile
 
 class Cell:
-    def __init__(self, multiplier, multiplier_type):
+    def __init__(self, multiplier, multiplier_type,activate=True):
         self.multiplier = multiplier
         self.multiplier_type = multiplier_type
-        self.letter = None
+        self.letter = None  
+        self.activate= activate
 
-    def add_letter(self, letter):
+    def add_letter(self, letter:Tile):
         self.letter = letter
 
     def calculate_value(self):
@@ -17,8 +18,19 @@ class Cell:
             return self.letter.value * self.multiplier
         else:
             return self.letter.value
+        
 
 class Board:
     def __init__(self):
         self.grid = [[ Cell(1, '') for _ in range(15) ]
         for _ in range(15)]
+    
+    def calculate_word_value(self, word):
+        value = 0
+        for cell in word:
+            value += cell.calculate_value()
+        for cell in word:
+            if cell.multiplier_type == 'word':
+                value *= cell.multiplier
+                cell.multiplier = 1
+        return value
--- tests/test_board.py
+++ tests/test_board.py
@@ -1,5 +1,6 @@
 import unittest
 from game.board import *
+from game.tilebag import Tile
 
 class TestCell(unittest.TestCase):
     def test_init(self):
@@ -28,10 +29,67 @@ class TestCell(unittest.TestCase):
         cell.add_letter(letter=letter)
         self.assertEqual(cell.calculate_value(),3)
 
+
 class TestBoard(unittest.TestCase):
     def test_init(self):
         board = Board()
         self.assertEqual(len(board.grid),15)
         self.assertEqual(len(board.grid[0]),15)
+
+    def test_simple(self):
+        cell_1 = Cell(multiplier=1,multiplier_type="letter")
+        cell_1.add_letter(Tile('C', 1)) 
+        cell_2 = Cell(multiplier=1,multiplier_type="letter")
+        cell_2.add_letter(Tile('A', 1))
+        cell_3 = Cell(multiplier=1,multiplier_type="letter")
+        cell_3.add_letter(Tile('S', 2))
+        cell_4 = Cell(multiplier=1,multiplier_type="letter")
+        cell_4.add_letter(Tile('A', 1))
+
+        word = [cell_1, cell_2, cell_3, cell_4]
+        value = Board().calculate_word_value(word)
+        self.assertEqual(value, 5)
+    
+    def test_letter_multiplier(self):
+        cell_1 = Cell(multiplier=1,multiplier_type="letter")
+        cell_1.add_letter(Tile('C', 1)) 
+        cell_2 = Cell(multiplier=1,multiplier_type="letter")
+        cell_2.add_letter(Tile('A', 1))
+        cell_3 = Cell(multiplier=2,multiplier_type="letter")
+        cell_3.add_letter(Tile('S', 2))
+        cell_4 = Cell(multiplier=1,multiplier_type="letter")
+        cell_4.add_letter(Tile('A', 1))
+
+        word = [cell_1, cell_2, cell_3, cell_4]
+        value = Board().calculate_word_value(word)
+        self.assertEqual(value, 7)
+    
+    def test_word_multiplier(self):
+        cell_1 = Cell(multiplier=1,multiplier_type="letter")
+        cell_1.add_letter(Tile('C', 1)) 
+        cell_2 = Cell(multiplier=1,multiplier_type="letter")
+        cell_2.add_letter(Tile('A', 1))
+        cell_3 = Cell(multiplier=2,multiplier_type="word")
+        cell_3.add_letter(Tile('S', 2))
+        cell_4 = Cell(multiplier=1,multiplier_type="letter")
+        cell_4.add_letter(Tile('A', 1))
+
+        word = [cell_1, cell_2, cell_3, cell_4]
+        value = Board().calculate_word_value(word)
+        self.assertEqual(value, 10)
+
+    def test_letter_and_word_multiplier(self):
+        cell_1 = Cell(multiplier=2,multiplier_type="letter")
+        cell_1.add_letter(Tile('C', 1)) 
+        cell_2 = Cell(multiplier=1,multiplier_type="letter")
+        cell_2.add_letter(Tile('A', 1))
+        cell_3 = Cell(multiplier=2,multiplier_type="word")
+        cell_3.add_letter(Tile('S', 2))
+        cell_4 = Cell(multiplier=1,multiplier_type="letter")
+        cell_4.add_letter(Tile('A', 1))
+
+        word = [cell_1, cell_2, cell_3, cell_4]
+        value = Board().calculate_word_value(word)
+        self.assertEqual(value, 12)
 if __name__ == '__main__':
     unittest.main()
