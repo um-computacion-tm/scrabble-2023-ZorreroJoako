7e69b5521e9062964a7e71120b3180925195d9d6
Zorrero Joaquín <j.zorrero@alumno.um.edu.ar>
2023-09-12 21:09:37 -0300

    Reorganize code

--- game/board.py
+++ game/board.py
@@ -1,25 +1,6 @@
-
 from game.tilebag import Tile
-
-class Cell:
-    def __init__(self, multiplier, multiplier_type,activate=True):
-        self.multiplier = multiplier
-        self.multiplier_type = multiplier_type
-        self.letter = None  
-        self.activate= activate
-
-    def add_letter(self, letter:Tile):
-        self.letter = letter
-
-    def calculate_value(self):
-        if self.letter is None:
-            return 0
-        if self.multiplier_type == 'letter':
-            return self.letter.value * self.multiplier
-        else:
-            return self.letter.value
-        
-
+from game.tilebag import Wildcard
+from game.cell import Cell
 class Board:
     def __init__(self):
         self.grid = [[ Cell(1, '') for _ in range(15) ]
@@ -34,3 +15,11 @@ class Board:
                 value *= cell.multiplier
                 cell.multiplier = 1
         return value
+
+    def validate_len_word(self, word, location_x, location_y,orientation):
+        self.location_x= location_x
+        self.location_y= location_y
+        len_word = len(word)
+        
+
+            
--- tests/test_board.py
+++ tests/test_board.py
@@ -1,33 +1,7 @@
 import unittest
 from game.board import *
 from game.tilebag import Tile
-
-class TestCell(unittest.TestCase):
-    def test_init(self):
-        cell = Cell(multiplier=2, multiplier_type='letter')
-        self.assertEqual(cell.multiplier,2)
-        self.assertEqual(cell.multiplier_type,'letter')
-        self.assertIsNone(cell.letter)
-        self.assertEqual(
-            cell.calculate_value(),0)
-
-    def test_add_letter(self):
-        cell = Cell(multiplier=1, multiplier_type='')
-        letter = Tile(letter='p', value=3)
-        cell.add_letter(letter=letter)
-        self.assertEqual(cell.letter, letter)
-
-    def test_cell_value(self):
-        cell = Cell(multiplier=2, multiplier_type='letter')
-        letter = Tile(letter='p', value=3)
-        cell.add_letter(letter=letter)
-        self.assertEqual(cell.calculate_value(),6)
-
-    def test_cell_multiplier_word(self):
-        cell = Cell(multiplier=2, multiplier_type='word')
-        letter = Tile(letter='p', value=3)
-        cell.add_letter(letter=letter)
-        self.assertEqual(cell.calculate_value(),3)
+from game.cell  import Cell
 
 
 class TestBoard(unittest.TestCase):
@@ -91,5 +65,21 @@ class TestBoard(unittest.TestCase):
         word = [cell_1, cell_2, cell_3, cell_4]
         value = Board().calculate_word_value(word)
         self.assertEqual(value, 12)
+
+    def test_word_inside_board(self):
+        board = Board()
+        word = "Facultad"
+        location = (5, 4)
+        orientation = "H"
+        word_is_valid = board.validate_word_inside_board(word, location, orientation)
+        assert word_is_valid == True
+
+    def test_word_out_of_board(self):
+        board = Board()
+        word = "Facultad"
+        location = (14, 4)
+        orientation = "H"
+        word_is_valid = board.validate_word_inside_board(word, location, orientation)
+        assert word_is_valid == False
 if __name__ == '__main__':
     unittest.main()
