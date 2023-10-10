cf0bf17289d8022ac6c67f532ffafc74f0e55998
Zorrero Joaquín <j.zorrero@alumno.um.edu.ar>
2023-10-09 21:07:49 -0300

    Implement main start and his tests

--- game/main.py
+++ game/main.py
@@ -4,22 +4,25 @@ class ValueError(Exception):
     pass
 class Main():
     def main(self):
-        print("Welcome to Scrabble")
+        print("Bienvenido a Scrable")
         while True:
             try:
-                players_count = int(input("Enter the number of players (2-4)"))
+                players_count = int(input("Ingrese el numero de jugadores (2-4)"))
                 if players_count <=1   or players_count > 4:
                     raise ValueError
                 break
             except ValueError:
                 print ("Valor no valido")
-        pass
+        return players_count
+
+    
+    
         scrabble_game = ScrabbleGame(players_count=players_count)
         print ("Cantidad de jugadores: ", len(scrabble_game.players))
         scrabble_game.next_turn()
         print ("Turno de: ", scrabble_game.current_player)
-        word= input("Enter a Word: ")
-        location_x= input ("Enter x coordinate: ")
-        location_y= input ("Enter y coordinate: ")
+        word= input("Ingrese una palabra: ")
+        location_x= input ("Ingrese la coordenada x: ")
+        location_y= input ("Ingrese la coordenada y: ")
         location = [location_x, location_y]
-        orientation = input ("Enter orientation (V/H): ")
+        orientation = input ("Ingrese la orientación (V/H): ")
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,22 @@
+import unittest
+from unittest.mock import patch
+from game.main import *
 
-    
+class TestMain(unittest.TestCase):
+    def test_main(self):
+        with patch('builtins.input', side_effect=[2]):
+            main = Main()
+            self.assertEqual(main.main(), 2)
+
+    def test_main_fail(self):
+        with patch('builtins.input', side_effect=[1, 5, 2]):
+            main = Main()
+            self.assertEqual(main.main(), 2)
+
+    def test_main_fail2(self):
+        with patch('builtins.input', side_effect=[1, 5, 2]):
+            main = Main()
+            self.assertNotEqual(main.main(), 1)
         
+if __name__ == '__main__':
+    unittest.main()
