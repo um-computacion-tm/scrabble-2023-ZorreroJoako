02ad47c7433b195c5f17d8a56005b12e7ffe8a56
Zorrero Joaquín <j.zorrero@alumno.um.edu.ar>
2023-10-10 22:37:28 -0300

    Try to implemente menu

--- game/main.py
+++ game/main.py
@@ -15,14 +15,48 @@ class Main():
                 print ("Valor no valido")
         return players_count
 
-    
-    
-        scrabble_game = ScrabbleGame(players_count=players_count)
-        print ("Cantidad de jugadores: ", len(scrabble_game.players))
-        scrabble_game.next_turn()
-        print ("Turno de: ", scrabble_game.current_player)
-        word= input("Ingrese una palabra: ")
-        location_x= input ("Ingrese la coordenada x: ")
-        location_y= input ("Ingrese la coordenada y: ")
-        location = [location_x, location_y]
-        orientation = input ("Ingrese la orientación (V/H): ")
+    def menu(self, menu):
+        if menu == 'menu':
+            return '''    Menu
+    1) Ver tablero
+    2) Ver Atril
+    3) Ver Acciones
+    4) Ver Puntuaciones
+    Seleccion: '''
+        elif menu == 'actions':
+            return '''    Acciones
+    1) Colocar palabra
+    2) Pasar turno
+    3) Cambiar fichas
+    4) Salir
+    Seleccion: '''  
+        elif menu == 'scores':
+            return '''    Puntuaciones
+    1) Ver puntuaciones
+    2) Salir
+    Seleccion: '''
+        elif menu == 'board':
+            return '''    Tablero
+    1) Ver tablero
+    2) Salir
+    Seleccion: '''
+        elif menu == 'rack':
+            return '''    Atril
+    1) Ver atril
+    2) Salir
+    Seleccion: '''
+        elif menu == 'change_tiles':
+            return '''    Cambiar fichas
+    1) Cambiar fichas
+    2) Salir
+    Seleccion: '''
+        
+        # scrabble_game = ScrabbleGame(players_count=players_count)
+        # print ("Cantidad de jugadores: ", len(scrabble_game.players))
+        # scrabble_game.next_turn()
+        # print ("Turno de: ", scrabble_game.current_player)
+        # word= input("Ingrese una palabra: ")
+        # location_x= input ("Ingrese la coordenada x: ")
+        # location_y= input ("Ingrese la coordenada y: ")
+        # location = [location_x, location_y]
+        # orientation = input ("Ingrese la orientación (V/H): ")
--- tests/test_main.py
+++ tests/test_main.py
@@ -17,6 +17,52 @@ class TestMain(unittest.TestCase):
         with patch('builtins.input', side_effect=[1, 5, 2]):
             main = Main()
             self.assertNotEqual(main.main(), 1)
+
+    def test_menu(self):
+        main = Main()
+        self.assertEqual(main.menu('menu'), '''    Menu
+    1) Ver tablero
+    2) Ver Atril
+    3) Ver Acciones
+    4) Ver Puntuaciones
+    Seleccion: ''')
+
+    def test_menu_actions(self):
+        main = Main()
+        self.assertEqual(main.menu('actions'), '''    Acciones
+    1) Colocar palabra
+    2) Pasar turno
+    3) Cambiar fichas
+    4) Salir
+    Seleccion: ''')
+
+    def test_menu_scores(self):
+        main = Main()
+        self.assertEqual(main.menu('scores'), '''    Puntuaciones
+    1) Ver puntuaciones
+    2) Salir
+    Seleccion: ''')
+
+    def test_menu_board(self):
+        main = Main()
+        self.assertEqual(main.menu('board'), '''    Tablero
+    1) Ver tablero
+    2) Salir
+    Seleccion: ''')
+
+    def test_menu_rack(self):
+        main = Main()
+        self.assertEqual(main.menu('rack'), '''    Atril
+    1) Ver atril
+    2) Salir
+    Seleccion: ''')
+
+    def test_menu_change_tiles(self):
+        main = Main()
+        self.assertEqual(main.menu('change_tiles'), '''    Cambiar fichas
+    1) Cambiar fichas
+    2) Salir
+    Seleccion: ''')
         
 if __name__ == '__main__':
     unittest.main()
